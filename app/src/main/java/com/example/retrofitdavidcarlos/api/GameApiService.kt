133eea02c1ab3007cc9596f6a115ff0c4fc2f1cb
XCDavidXD2 <davidgustavo.correa@gracia.lasalle.cat>
2025-02-14 12:02:45 +0100

    fix: correccion de errores

--- app/src/main/java/com/example/retrofitdavidcarlos/api/GameApiService.kt
+++ app/src/main/java/com/example/retrofitdavidcarlos/api/GameApiService.kt
@@ -7,16 +7,18 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface GameApiService {
 
-    @GET("/games")
+    @GET("games")
     suspend fun getAllGames(
-        @Header("Authorization") token: String
+        @Query("key") apiKey: String = "244ec649ea554979b3cfb39c90ee1100"
     ): Response<GameResponse>
 
     companion object {
-        val BASE_URL = "https://api.rawg.io/api/"
+        private const val BASE_URL = "https://api.rawg.io/api/"
+
         fun create(): GameApiService {
             val client = OkHttpClient.Builder().build()
             val retrofit = Retrofit.Builder()
