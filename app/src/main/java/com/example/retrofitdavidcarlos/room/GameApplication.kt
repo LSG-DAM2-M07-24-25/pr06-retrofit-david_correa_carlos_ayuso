172bb0dfbde0a24677245386ba9d93dc0c599f0c
Carlos Ayuso <carlos.ayuso@gracia.lasalle.cat>
2025-02-14 17:03:27 +0100

    add: Create GameApplication

---
+++ app/src/main/java/com/example/retrofitdavidcarlos/room/GameApplication.kt
@@ -0,0 +1,27 @@
+package com.example.retrofitdavidcarlos.room
+
+import android.app.Application
+import androidx.room.Room
+
+/**
+ * Aquesta classe està pensada per tal de que només se'n pugui instanciar
+ * un únic objecte en tot el present projecte, comportant-se com una classe estàtica
+ * Això s'anomena patró Singleton (que vol dir solitari o únic) i fa referència a
+ * que només se'n crea un objecte. Aquest objecte únic és global i accessible areu del programa.
+ * És útil per a gestionar recursos compartits com per exemple una connexió a BD, una API, etc.
+ *
+ * S'executarà només començar l'aplicació ja que està dins de AndroidManifest.xml i crearà la BD
+ */
+class GameApplication: Application() {
+
+    // Creem un atribut estàtic de la classe
+    companion object {
+        lateinit var database: GameDatabase
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        // Aquí determinem el nom de la base de dades. En el nostre cas "PokemonDatabase"
+        database = Room.databaseBuilder(this, GameDatabase::class.java,"GameDatabase").build()
+    }
+}
