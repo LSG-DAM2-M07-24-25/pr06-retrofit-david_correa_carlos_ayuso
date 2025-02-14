670b80c54dc023cb5fee131cf37f896347413692
XCDavidXD2 <davidgustavo.correa@gracia.lasalle.cat>
2025-02-14 11:58:57 +0100

    fix: modificaciones de la api

--- app/src/main/java/com/example/retrofitdavidcarlos/api/Repository.kt
+++ app/src/main/java/com/example/retrofitdavidcarlos/api/Repository.kt
@@ -2,6 +2,6 @@ package com.example.retrofitdavidcarlos.api
 
 class Repository {
     val apiInterface = GameApiService.create()
-    val token = "244ec649ea554979b3cfb39c90ee1100"
-    suspend fun getAllGames() = apiInterface.getAllGames(token)
+
+    suspend fun getAllGames() = apiInterface.getAllGames()
 }
