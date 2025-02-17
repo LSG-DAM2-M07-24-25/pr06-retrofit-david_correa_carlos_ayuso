376cbd07cec8cfa27028c1bce2bb9ae137fa8fa1
Carlos Ayuso <carlos.ayuso@gracia.lasalle.cat>
2025-02-17 18:29:07 +0100

    fix:  @Database

--- app/src/main/java/com/example/retrofitdavidcarlos/room/GameDatabase.kt
+++ app/src/main/java/com/example/retrofitdavidcarlos/room/GameDatabase.kt
@@ -1,7 +1,10 @@
 package com.example.retrofitdavidcarlos.room
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.retrofitdavidcarlos.model.Game
 
-abstract class GameDatabase: RoomDatabase() {
+@Database(entities = [Game::class], version = 3)
+abstract class GameDatabase : RoomDatabase() {
     abstract fun GameDao(): GameDao
 }
