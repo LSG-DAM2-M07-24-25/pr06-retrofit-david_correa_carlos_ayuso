c1616dae02a0a492b93d110e96fe8457ad6ceae8
XCDavidXD2 <davidgustavo.correa@gracia.lasalle.cat>
2025-03-13 11:46:07 +0100

    feat: historial dao consultas extra

--- app/src/main/java/com/example/retrofitdavidcarlos/room/HistorialDao.kt
+++ app/src/main/java/com/example/retrofitdavidcarlos/room/HistorialDao.kt
@@ -9,7 +9,7 @@ import com.example.retrofitdavidcarlos.model.Busqueda
 @Dao
 interface HistorialDao {
 
-    @Query("SELECT * FROM historial")
+    @Query("SELECT * FROM historial ORDER BY timestamp DESC")
     fun getAllQueries(): List<Busqueda>
 
     @Insert
@@ -18,7 +18,12 @@ interface HistorialDao {
     @Query("DELETE FROM historial")
     fun borrarHistorial()
 
-    @Query("SELECT `query` FROM historial WHERE `query` = :busqueda ")
+    @Query("SELECT `query` FROM historial WHERE `query` = :busqueda")
     fun findByQuery(busqueda: String): List<String>
 
+    @Query("DELETE FROM historial WHERE `query` = :query")
+    fun deleteQuery(query: String)
+
+    @Query("UPDATE historial SET timestamp = :timestamp WHERE `query` = :query")
+    fun updateTimestamp(query: String, timestamp: Long)
 }
